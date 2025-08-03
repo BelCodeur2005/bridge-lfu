f9380dd2546152e98679e65d013cb2da0d708a78
BelCodeur <cruzjaydee15@gmail.com>
2025-08-03 17:15:31 +0100

    Premier 6 commit

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,26 +1,25 @@
-// lib/supabase/server.ts
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import type { GetServerSidePropsContext } from 'next';
 import type { Database } from '@/types/database';
 
-export const createSupabaseServerClient = () => {
-  const cookieStore = cookies();
-
+export const createSupabaseServerClient = (context: GetServerSidePropsContext) => {
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll: async () => {
-          return (await cookieStore).getAll().map((cookie) => ({
-            name: cookie.name,
-            value: cookie.value,
-          }));
+        get: (name: string) => {
+          return context.req.cookies[name];
         },
-        setAll: async (cookiesToSet) => {
-          cookiesToSet.forEach(async (cookie) => {
-            (await cookieStore).set(cookie);
-          });
+        set: (name: string, value: string, options: CookieOptions) => {
+          context.res.setHeader('Set-Cookie', (context.res.getHeader('Set-Cookie') as string[] || []).concat(
+            `${name}=${value}; Path=${options.path}; Max-Age=${options.maxAge}; HttpOnly; Secure`
+          ));
+        },
+        remove: (name: string, options: CookieOptions) => {
+          context.res.setHeader('Set-Cookie', (context.res.getHeader('Set-Cookie') as string[] || []).concat(
+            `${name}=; Path=${options.path}; Max-Age=0; HttpOnly; Secure`
+          ));
         },
       },
     }
